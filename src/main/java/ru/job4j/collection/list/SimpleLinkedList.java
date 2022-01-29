6b9e7592db44fce171f10fc2b29ab52e10fd69b2
Alex <zlovred2k@gmail.com>
2022-01-29 15:54:09 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,85 @@
+package ru.job4j.collection.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> first;
+    private Node<E> last;
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> current = first;
+        int count = 0;
+        while (count != index) {
+            current = current.next;
+            count++;
+        }
+        return current.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            private final int expectedModCount = modCount;
+            private int count = 0;
+            Node<E> current = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (count == 0) {
+                    current = first;
+                } else {
+                    current = current.next;
+                }
+                count++;
+                return current.item;
+            }
+        };
+    }
+}
