48a6963dc7b2c45d2576bbdf5e90affb770bf717
Alex <zlovred2k@gmail.com>
2022-11-29 23:01:31 +0300

    2. Анализ доступности сервера. [#859 #268563]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -3,7 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analizy {
-    //split[0].equals("500") "500".equals(split[0])
+
     public void unavailable(String source, String target) {
         StringBuilder log = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
